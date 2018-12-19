aac30f24d41cea8a4ad676495bf39e0d31d70119
Stanislav Poslavsky <stvlpos@mail.ru>
2018-12-20 02:00:51 +0300

    Update MMA installation script

--- mma/Install.m
+++ mma/Install.m
@@ -19,7 +19,7 @@ DoInstall[OptionsPattern[]] := Module[{zip, unzipDir},
   zip = $GetUrl[$GithubRelease];
   unzipDir = zip <> ".dir";
   CreateDirectory[unzipDir];
-  ExtractArchive[unzipDir, toPath];
+  ExtractArchive[unzipDir, OptionValue[InstallTo]];
   CopyDirectory[FileNameJoin[{unzipDir, "rings.oneloop-" <> $OneloopVersion}], OptionValue[InstallTo]];
 
   Quiet@DeleteFile[zip];
